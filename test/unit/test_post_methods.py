e37009a9a281002c63bb3cca8882a94b3af1e852
Sobirjon005 <sobirjon0305@gmail.com>
2025-03-12 09:32:13 +0500

    added fail tests

--- test/unit/test_post_methods.py
+++ test/unit/test_post_methods.py
@@ -7,11 +7,25 @@ class TestRoomTypePostEndpoint:
         "payload, expected_status",
         [
             ({"title": "Deluxe Suite"}, status.HTTP_201_CREATED),
-            ({}, status.HTTP_422_UNPROCESSABLE_ENTITY),
             ({"title": "Standard Room"}, status.HTTP_201_CREATED),
-            ({"title": "A"}, status.HTTP_400_BAD_REQUEST),
         ]
     )
     def test_create_room_type(self, http_client, payload, expected_status):
         response = http_client.post("/room_types/", json=payload)
         assert response.status_code == expected_status
+        assert response.json().get("title") == payload["title"]
+
+    @pytest.mark.parametrize(
+        "payload, expected_status",
+        [
+            ({}, status.HTTP_422_UNPROCESSABLE_ENTITY),
+            ({"title": "A"}, status.HTTP_400_BAD_REQUEST),
+
+        ]
+    )
+    def test_create_room_type_fail(self, http_client, payload, expected_status):
+        response = http_client.post("/room_types/", json=payload)
+        assert response.status_code == expected_status
+        assert response.json().get("title") is None
+
+
