6b089d3bc5cfcfd761ccf3e2486ea5ad0b436a2f
Shaxzodbek <muxtorovshaxzodbek16@gmail.com>
2025-03-12 10:06:14 +0500

    room type tests were done

--- test/unit/test_endpoints.py
+++ test/unit/test_endpoints.py
@@ -1,5 +1,3 @@
-from http.client import responses
-
 import pytest
 from fastapi import status
 from datetime import datetime
@@ -101,3 +99,67 @@ class TestEndpoints:
     def test_post_booking_failed(self, http_client, data, expected, status_code):
         response = http_client.post('booking/', json=data)
         assert response.status_code == status_code
+
+    @pytest.mark.parametrize(
+        "payload, expected_status",
+        [
+            ({"title": "Deluxe Suite"}, status.HTTP_201_CREATED),
+            ({"title": "Standard Room"}, status.HTTP_201_CREATED),
+        ]
+    )
+    def test_create_room_type(self, http_client, payload, expected_status):
+        response = http_client.post("/room_types/", json=payload)
+        assert response.status_code == expected_status
+        assert response.json().get("title") == payload["title"]
+
+    @pytest.mark.parametrize(
+        "payload, expected_status",
+        [
+            ({}, status.HTTP_422_UNPROCESSABLE_ENTITY),
+            ({"title": "A"}, status.HTTP_400_BAD_REQUEST),
+
+        ]
+    )
+    def test_create_room_type_fail(self, http_client, payload, expected_status):
+        response = http_client.post("/room_types/", json=payload)
+        assert response.status_code == expected_status
+        assert response.json().get("title") is None
+
+    @pytest.mark.parametrize(
+        "room_type_id, payload, expected_status",
+        [
+            (1, {"title": "Updated Suite"}, status.HTTP_200_OK)
+        ]
+    )
+    def test_put_room_type(self, http_client, room_type_id, payload, expected_status):
+        response = http_client.put(f"/room_types/{room_type_id}/", json=payload)
+        assert response.status_code == expected_status
+        assert response.json().get("title") == payload["title"]
+
+    @pytest.mark.parametrize(
+        "room_type_id, payload, expected_status",
+        [
+            (999, {"title": "Nonexistent Room"}, status.HTTP_404_NOT_FOUND),
+            (1, {}, status.HTTP_422_UNPROCESSABLE_ENTITY),
+            (1, {"name": "test"}, status.HTTP_422_UNPROCESSABLE_ENTITY),
+        ]
+    )
+    def test_delete_room_type_fail(self, http_client, room_type_id, payload, expected_status):
+        response = http_client.put(f"/room_types/{room_type_id}/", json=payload)
+        assert response.status_code == expected_status
+        assert response.json().get("title") is None
+
+    @pytest.mark.parametrize(
+        "path, expected_status",
+        [
+            ("/rooms/", status.HTTP_200_OK),
+            ("/test/", status.HTTP_404_NOT_FOUND),
+            ("/room_types/1", status.HTTP_200_OK),
+            ("/rooms/999/", status.HTTP_404_NOT_FOUND),
+            ("/rooms/1i/", status.HTTP_422_UNPROCESSABLE_ENTITY),
+            ("/room_types/999/", status.HTTP_404_NOT_FOUND),
+        ]
+    )
+    def test_rooms(self, http_client, path, expected_status):
+        response = http_client.get(path)
+        assert response.status_code == expected_status
