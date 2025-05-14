9946dd7f3316059dfc9debfb8ce6fd7bf0ed8403
BotirIslomboev <=>
2025-05-14 14:41:25 +0500

    feat(core): 1. remove rq add celery

--- app/api/controller/rooms.py
+++ app/api/controller/rooms.py
@@ -1,7 +1,8 @@
-from fastapi import Depends, HTTPException
-from starlette import status
+from fastapi import Depends
 
 from app.api.repositories.rooms import RoomsRepository
+from app.api.schemas.rooms import RoomsCreateSchema
+from app.api.tasks.rooms import update_creating_rooms
 
 
 class RoomsController:
@@ -24,3 +25,13 @@ class RoomsController:
         #         }
         #     )
         return items
+
+    async def create_room(self, body: RoomsCreateSchema):
+        celery_tasks_id = await self.__rooms_repo.create_room(
+            room_number=body.room_number,
+            room_type=body.room_type,
+            price=body.price,
+            status=body.status
+        )
+        update_creating_rooms.delay(task_id=str(celery_tasks_id))
+        return celery_tasks_id
--- app/api/repositories/rooms.py
+++ app/api/repositories/rooms.py
@@ -1,18 +1,26 @@
-from fastapi import Depends, HTTPException, status
+import uuid
+import time
 
-from sqlalchemy import text
+from fastapi import Depends, HTTPException
+
+from sqlalchemy import text, insert, update
 from sqlalchemy.ext.asyncio import AsyncSession
-from app.core.database.config import get_general_session
+from sqlalchemy.orm import Session
+
+from app.core.database.config import get_general_session, get_general_sync_session, get_sync_session_maker
 
 from app.api.schemas.rooms import RoomsSchema
+from app.api.models.rooms import Rooms
 
 
 class RoomsRepository:
     def __init__(
             self,
-            session: AsyncSession = Depends(get_general_session),
+            sync_session: Session = Depends(get_sync_session_maker),
+            session: AsyncSession = Depends(get_general_session)
     ):
         self.session = session
+        self.sync_session = sync_session
 
     async def get_all_rooms(self):
         raw_sql = text("""
@@ -34,7 +42,42 @@ class RoomsRepository:
         """).bindparams(room_id=room_id)
 
         stmt = await self.session.execute(raw_sql)
-        res  = stmt.mappings().first()
+        res = stmt.mappings().first()
         if res is None:
             raise HTTPException(status_code=404, detail="Room not found")
         return RoomsSchema.model_validate(res)
+
+    async def create_room(
+            self,
+            room_number: int,
+            room_type: int,
+            price: float,
+            status: int
+    ):
+        celery_tasks_uuid = uuid.uuid4()
+        stmt = insert(Rooms).values(
+            room_number=room_number,
+            room_type=room_type,
+            price=price,
+            status=status,
+            celery_tasks=celery_tasks_uuid,
+            tasks_status="PENDING"
+        )
+        await self.session.execute(stmt)
+        await self.session.commit()
+        return celery_tasks_uuid
+
+    def update_room(
+            self,
+            task_id: str
+    ):
+        time.sleep(10)
+        self.sync_session.execute(
+            update(Rooms).where(Rooms.celery_tasks == task_id).values(tasks_status="PROCESSING")
+        )
+        self.sync_session.commit()
+        time.sleep(20)
+        self.sync_session.execute(
+            update(Rooms).where(Rooms.celery_tasks == task_id).values(tasks_status="DONE")
+        )
+        self.sync_session.commit()
